140893cda9c9133aabc15e882c36cd61d945277d
Ryan Cutrone <ryan.cutrone@gmail.com>
2026-09-08 18:13:08 -0600

    remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './scss/styles.scss'
-import * as bootstrap from 'bootstrap'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
