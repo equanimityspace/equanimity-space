f6b726d065caadd7f47a8b52bc1d291fa84325ee
Ryan Cutrone <ryan.cutrone@gmail.com>
2026-09-08 18:06:38 -0600

    Add twinkling stars bg

---
+++ src/components/Stars.tsx
@@ -0,0 +1,43 @@
+import '../App.css';
+
+function Stars() {
+  // create star field
+  const stars: number[] = [];
+  for (let i = 0; i < 1000; i++) {
+    stars.push(i);
+  }
+
+  //   for each num in arr stars, create a unique div with a random position for the small star
+  return (
+    <>
+      {stars.map((star) => {
+        const styleTop: string = Math.random() * 100 + "%";
+        const styleLeft: string = Math.random() * 100 + "%";
+
+	// Random star twinkle
+	const animDuration: string = Math.random() * 3 + 2 + "s";
+	const animDelay: string = Math.random() * 5 + "s";
+
+        return (
+          <div
+            key={star}
+            className="bg-white star-twinkle"
+            style={{
+	      position: "absolute",
+              width: "1px",
+              height: "1px",
+              top: styleTop,
+              left: styleLeft,
+	      pointerEvents: "none",
+	      animationDuration: animDuration,
+	      animationDelay: animDelay,
+	      opacity: 0.1, // Render with minimum opacity to avoid "popping" effect for first 5s
+            }}
+          ></div>
+        );
+      })}
+    </>
+  );
+}
+
+export default Stars;
