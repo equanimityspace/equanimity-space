3345985336b4ff6d2b5fbb48bc8bc60f018ceb20
Ryan Cutrone <ryan.cutrone@gmail.com>
2026-09-08 21:41:17 -0600

    Add rocket that follows viewport while scrolling down

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Stars from './components/Stars'
+import Stars from "./components/Stars";
+import Rocket from "./components/Rocket";
 
 function App() {
   return (
-    <div className="container-fluid bg-black min-vh-100 vw-100 p-0 position-relative">
+    <div className="space-container container-fluid bg-black min-vh-100 vw-100 p-0 position-relative">
       <Stars />
+      <Rocket />
     </div>
   );
 }
