9610d755f979bebb88174afc40441a7452993172
Ryan Cutrone <ryan.cutrone@gmail.com>
2026-09-12 18:26:36 -0600

    create About component

---
+++ src/components/About.tsx
@@ -0,0 +1,29 @@
+// This file is part of equanimity.space.
+//
+//    equanimity.space is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    equanimity.space is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with equanimity.space.  If not, see <https://www.gnu.org/licenses/>.
+
+function About() {
+  return (
+    <div className="py-5">
+      <div className="row mb-5">
+        <div className="col-12 text-center">
+          <h2 className="display-5 fw-bold text-white">About Me</h2>
+          <hr className="w-25 mx-auto border-purple" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
